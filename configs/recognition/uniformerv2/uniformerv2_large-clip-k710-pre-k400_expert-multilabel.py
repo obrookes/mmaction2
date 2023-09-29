ab3386a56d0aa38ddc3f5b2e30da3f3f27b41e4e
obrookes <dl18206@bristol.ac.uk>
2023-09-29 16:30:19 +0100

    map added to wandb

--- configs/recognition/uniformerv2/uniformerv2_large-clip-k710-pre-k400_expert-multilabel.py
+++ configs/recognition/uniformerv2/uniformerv2_large-clip-k710-pre-k400_expert-multilabel.py
@@ -162,7 +162,7 @@ param_scheduler = [
     ),
 ]
 
-train_cfg = dict(type="EpochBasedTrainLoop", max_epochs=24, val_begin=1, val_interval=1)
+train_cfg = dict(type="EpochBasedTrainLoop", max_epochs=4, val_begin=1, val_interval=1)
 val_cfg = dict(type="ValLoop")
 test_cfg = dict(type="TestLoop")
 
