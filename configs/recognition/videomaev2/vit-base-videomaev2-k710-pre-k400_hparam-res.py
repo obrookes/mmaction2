5ad972e0927620d3b0449877d4ae93882eb38ea9
obrookes <dl18206@bristol.ac.uk>
2023-10-09 11:17:30 +0100

    inc. res hparam config

--- configs/recognition/videomaev2/vit-base-videomaev2-k710-pre-k400_hparam-res.py
+++ configs/recognition/videomaev2/vit-base-videomaev2-k710-pre-k400_hparam-res.py
@@ -11,7 +11,7 @@ ann_file_test = "/jmain02/home/J2AD001/wwp02/oxb63-wwp02/data/camera_reaction/an
 file_client_args = dict(io_backend="disk")
 
 num_frames = 16
-batch_size = 4
+batch_size = 2
 num_classes = 2
 base_batch_size = 512
 
