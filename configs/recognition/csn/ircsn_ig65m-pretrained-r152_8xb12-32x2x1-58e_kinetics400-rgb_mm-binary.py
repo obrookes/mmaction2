2397d2cd1d955fc5a0058a5da48b02a76401c0cd
obrookes <dl18206@bristol.ac.uk>
2023-11-17 13:35:48 +0000

    config for csn on mm binary

---
+++ configs/recognition/csn/ircsn_ig65m-pretrained-r152_8xb12-32x2x1-58e_kinetics400-rgb_mm-binary.py
@@ -0,0 +1,164 @@
+_base_ = ["ircsn_ig65m-pretrained-r152-bnfrozen_8xb12-32x2x1-58e_kinetics400-rgb.py"]
+
+pretrained = "pretrained_models/ircsn_from_scratch_r152_ig65m_20200807-771c4135.pth"
+
+dataset_type = "VideoDataset"
+data_root = "/jmain02/home/J2AD001/wwp02/oxb63-wwp02/data/chimp_videos/all"
+ann_file_train = "/jmain02/home/J2AD001/wwp02/oxb63-wwp02/data/camera_reaction/annotations/mmaction2/mm_binary/train.txt"
+ann_file_val = "/jmain02/home/J2AD001/wwp02/oxb63-wwp02/data/camera_reaction/annotations/mmaction2/mm_binary/val.txt"
+ann_file_test = "/jmain02/home/J2AD001/wwp02/oxb63-wwp02/data/camera_reaction/annotations/mmaction2/mm_binary/test.txt"
+
+file_client_args = dict(io_backend="disk")
+
+num_frames = 16
+batch_size = 2
+num_classes = 2
+base_batch_size = 256
+accumulative_counts = 32
+
+model = dict(
+    backbone=dict(
+        pretrained=pretrained,
+    ),
+    cls_head=dict(
+        average_clips="prob",
+        dropout_ratio=0.5,
+        in_channels=2048,
+        init_std=0.01,
+        num_classes=2,
+        spatial_type="avg",
+        multi_class=True,
+        type="I3DHead",
+    ),
+    data_preprocessor=dict(
+        type="ActionDataPreprocessor",
+        mean=[114.75, 114.75, 114.75],
+        std=[57.375, 57.375, 57.375],
+        format_shape="NCTHW",
+    ),
+)
+
+optim_wrapper = dict(
+    clip_grad=dict(max_norm=40, norm_type=2),
+    optimizer=dict(lr=0.0005, momentum=0.9, type="SGD", weight_decay=0.0001),
+)
+
+param_scheduler = [
+    dict(begin=0, by_epoch=True, end=16, start_factor=0.1, type="LinearLR"),
+    dict(
+        begin=0,
+        by_epoch=True,
+        end=58,
+        gamma=0.1,
+        milestones=[
+            32,
+            48,
+        ],
+        type="MultiStepLR",
+    ),
+]
+
+# dataset settings
+
+train_pipeline = [
+    dict(type="DecordInit", **file_client_args),
+    dict(type="UniformSample", clip_len=num_frames, num_clips=1),
+    dict(type="DecordDecode"),
+    dict(type="Resize", scale=(-1, 256)),
+    dict(type="PytorchVideoWrapper", op="RandAugment", magnitude=7, num_layers=4),
+    dict(type="RandomResizedCrop"),
+    dict(type="Resize", scale=(224, 224), keep_ratio=False),
+    dict(type="Flip", flip_ratio=0.5),
+    dict(type="FormatShape", input_format="NCTHW"),
+    dict(type="PackActionInputs"),
+]
+
+val_pipeline = [
+    dict(type="DecordInit"),
+    dict(type="UniformSample", clip_len=num_frames, num_clips=1, test_mode=True),
+    dict(type="DecordDecode"),
+    dict(type="Resize", scale=(-1, 224)),
+    dict(type="ThreeCrop", crop_size=224),
+    dict(type="FormatShape", input_format="NCTHW"),
+    dict(type="PackActionInputs"),
+]
+
+test_pipeline = [
+    dict(type="DecordInit"),
+    dict(type="UniformSample", clip_len=num_frames, num_clips=1, test_mode=True),
+    dict(type="DecordDecode"),
+    dict(type="Resize", scale=(-1, 224)),
+    dict(type="ThreeCrop", crop_size=224),
+    dict(type="FormatShape", input_format="NCTHW"),
+    dict(type="PackActionInputs"),
+]
+
+train_dataloader = dict(
+    batch_size=batch_size,
+    num_workers=8,
+    persistent_workers=True,
+    sampler=dict(type="DefaultSampler", shuffle=True),
+    dataset=dict(
+        type=dataset_type,
+        ann_file=ann_file_train,
+        data_prefix=dict(video=data_root),
+        pipeline=train_pipeline,
+        num_classes=num_classes,
+        multi_class=True,
+    ),
+)
+val_dataloader = dict(
+    batch_size=batch_size,
+    num_workers=8,
+    persistent_workers=True,
+    sampler=dict(type="DefaultSampler", shuffle=False),
+    dataset=dict(
+        type=dataset_type,
+        ann_file=ann_file_val,
+        data_prefix=dict(video=data_root),
+        pipeline=val_pipeline,
+        test_mode=True,
+        num_classes=num_classes,
+        multi_class=True,
+    ),
+)
+
+test_dataloader = dict(
+    batch_size=1,
+    num_workers=8,
+    persistent_workers=True,
+    sampler=dict(type="DefaultSampler", shuffle=False),
+    dataset=dict(
+        type=dataset_type,
+        ann_file=ann_file_test,
+        data_prefix=dict(video=data_root),
+        pipeline=test_pipeline,
+        test_mode=True,
+        num_classes=num_classes,
+        multi_class=True,
+    ),
+)
+
+val_evaluator = dict(
+    type="AccMetric",
+    metric_list=("mean_average_precision"),
+)
+test_evaluator = val_evaluator
+
+train_cfg = dict(type="EpochBasedTrainLoop", max_epochs=48, val_interval=1)
+val_cfg = dict(type="ValLoop")
+test_cfg = dict(type="TestLoop")
+
+default_hooks = dict(
+    checkpoint=dict(
+        interval=5, max_keep_ckpts=1, save_best="acc/mean_average_precision"
+    )
+)
+
+auto_scale_lr = dict(enable=True, base_batch_size=base_batch_size)
+
+vis_backends = [dict(type="LocalVisBackend"), dict(type="WandbVisBackend")]
+visualizer = dict(
+    type="ActionVisualizer",
+    vis_backends=vis_backends,
+)
