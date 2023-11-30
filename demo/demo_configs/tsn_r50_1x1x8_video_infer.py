cef75eaf1294b76a3fbba321dd80f1ceea3d4aa7
obrookes <dl18206@bristol.ac.uk>
2023-11-30 09:55:04 +0000

    amend inference script

--- demo/demo_configs/tsn_r50_1x1x8_video_infer.py
+++ demo/demo_configs/tsn_r50_1x1x8_video_infer.py
@@ -6,21 +6,20 @@ dataset_type = 'VideoDataset'
 test_pipeline = [
     dict(type='DecordInit'),
     dict(
-        type='SampleFrames',
-        clip_len=1,
-        frame_interval=1,
-        num_clips=8,
+        type='UniformSampleFrames',
+        clip_len=32,
+        num_clips=1,
         test_mode=True),
     dict(type='DecordDecode'),
     dict(type='Resize', scale=(-1, 256)),
-    dict(type='CenterCrop', crop_size=224),
+    #dict(type='CenterCrop', crop_size=224),
     dict(type='FormatShape', input_format='NCHW'),
     dict(type='PackActionInputs')
 ]
 
 test_dataloader = dict(
     batch_size=1,
-    num_workers=2,
+    num_workers=1,
     dataset=dict(
         type=dataset_type,
         ann_file=None,
